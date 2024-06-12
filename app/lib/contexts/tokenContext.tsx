428dc5ee49b6f35dfb2e9f06c67a01a3751bf2d3
Gul Noor <67622150+gulnoor@users.noreply.github.com>
2024-06-13 01:11:19 +0500

    fixed token context

--- app/lib/contexts/tokenContext.tsx
+++ app/lib/contexts/tokenContext.tsx
@@ -4,23 +4,22 @@ import { createContext, useEffect, useState } from "react";
 export const TokenContext = createContext({ token: "" });
 
 const TokenProvider = ({ children }) => {
-  // function readToken() {
-  //   if (
-  //     typeof window !== "undefined" &&
-  //     !window.localStorage.getItem("theme")
-  //   ) {
-  //     window.localStorage.setItem("token", "");
-  //     return "";
-  //   } else if (typeof window !== "undefined") {
-  //     return window.localStorage.getItem("token");
-  //   }
-  // }
-
+  function getToken() {
+    if (
+      typeof window !== "undefined" &&
+      !window.localStorage.getItem("token")
+    ) {
+      // window.localStorage.setItem("token", "");
+      return "";
+    } else if (typeof window !== "undefined") {
+      return window.localStorage.getItem("token");
+    }
+    return "";
+  }
+//FIXME: refreshing any page adds dashboard link to navrail even if token is not set (actually it gets set to "null" as string maybe??)
   // TODO: use isClient state and verify token from server after reading it from local storage
-  const [token, setToken] = useState("");
-  useEffect(() => {
-    setToken(() => window.localStorage.getItem("token"));
-  }, []);
+  const [token, setToken] = useState(getToken());
+
   useEffect(() => {
     window.localStorage.setItem("token", token);
   }, [token]);
