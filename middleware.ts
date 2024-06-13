09388af64a486df9e8b4b3fcb759634f3ac9c1ca
Gul Noor <67622150+gulnoor@users.noreply.github.com>
2024-06-13 20:20:12 +0500

    added try catch to middleware

--- middleware.ts
+++ middleware.ts
@@ -9,35 +9,35 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   let token = request.headers.get("authorization");
   let body = {};
-  if (request.method === "POST") {
-    try {
+  try {
+    if (request.method === "POST") {
       body = await request.json();
-    } catch (err) {
-      return NextResponse.json({ error: err.message });
     }
-  }
-  console.log(
-    `-------------------------
-    Method: ${request.method}
-    Path: ${request.url}
-    Body: ${JSON.stringify(body)}
-    -----------------------`
-  );
-  const requestHeaders = new Headers(request.headers);
-  console.log("gg");
-  if (token && token.startsWith("Bearer ")) {
-    token = token.replace("Bearer ", "");
-  requestHeaders.set("authToken", token);
-  }
+    console.log(
+      `-------------------------
+      Method: ${request.method}
+      Path: ${request.url}
+      Body: ${JSON.stringify(body)}
+      -----------------------`
+    );
+    const requestHeaders = new Headers(request.headers);
     console.log("gg");
-    
+    if (token && token.startsWith("Bearer ")) {
+      token = token.replace("Bearer ", "");
+      requestHeaders.set("authToken", token);
+    }
+    console.log("gg");
+
     const modifiedRequest = new Request(request.url, {
       headers: requestHeaders,
-      });
+    });
     console.log("ff");
-return NextResponse.next({
-    request: modifiedRequest,
-  });
+    return NextResponse.next({
+      request: modifiedRequest,
+    });
+  } catch (err) {
+    return NextResponse.json({ error: err.message });
+  }
 }
 export const config = {
   matcher: ["/api/order", "/api/reservation"],
